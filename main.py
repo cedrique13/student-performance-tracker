80e1d1c9d0c17d3b3fd0e2cded3e3d1b67b84ac2
kamalafidele <kamalafizzet45@gmail.com>
2025-03-31 11:06:10 +0200

    ft: attendance

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from modules.report import Report
 def main():
     """CLI menu for the Student Performance Tracker"""
     while True:
-        print("1. Add Student\n2. List Students\n3. Mark Attendance\n4. Record Grade\n5. Generate Report\n6. Exit")
+        print("1. Add Student\n2. List Students\n3. Mark Attendance\n4. List attendance \n5. Record Grade\n6. Generate Report\n7. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
@@ -17,8 +17,14 @@ def main():
             Student.add_student(name, age, grade)
         elif choice == "2":
             Student.list_students()
+        elif choice == "3":
+            Attendance.mark_attendance()
+        elif choice == "4":
+            Attendance.list_attendance()
+        elif choice == "5":
+            Grades.record_grade()
 
-        elif choice == "6":
+        elif choice == "7":
             break
 
 
--- modules/attendance.py
+++ modules/attendance.py
@@ -3,9 +3,23 @@ from modules.database import Database
 
 class Attendance:
     @staticmethod
-    def mark_attendance(student_id, status):
+    def mark_attendance():
         """Record attendance for a student"""
         db = Database()
+        id = input("Student ID: ")
+        status = input("Stauts (Present/Absent): ")
+    
         query = "INSERT INTO attendance (student_id, date, status) VALUES (%s, CURDATE(), %s)"
-        db.execute_query(query, (student_id, status))
+        db.execute_query(query, (id, status))
+        db.close()
+        
+    def list_attendance():
+        """List attendance records"""
+        db = Database()
+        query = "SELECT * FROM attendance"
+        results = db.fetch_results(query)
+        for row in results:
+            # Print the results in a formatted way
+            print(f"Student ID: {row[1]}, Date: {row[2]}, Status: {row[3]}")
+            # print(row)
         db.close()
